94aaba5254c8fd5237308343cbbc7485ca1fe064
md sharffuddin <106474548+sharfuddin1986@users.noreply.github.com>
2023-09-15 03:38:07 +0530

    Delete linux_memcache_ready.sh

--- linux_memcache_ready.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-yum install epel-release -y
-yum install memcached -y
-systemctl start memcached
-systemctl enable memcached
-systemctl status memcached
-memcached -p 11211 -U 11111 -u memcached -d
